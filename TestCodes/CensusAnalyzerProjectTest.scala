8bc650e08c7d0d6fa77d7e6ccc6dc198fffe41f9
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-08-30 23:37:18 +0530

    Niraj[Refracted]:Scala test codes of Census Analyzer for UserCase 1.3

--- TestCodes/CensusAnalyzerProjectTest.scala
+++ TestCodes/CensusAnalyzerProjectTest.scala
@@ -8,12 +8,18 @@ class CensusAnalyzerProjectTest extends FunSuite{
     assert(objCensus.loadCSVData("./src/main/scala/CensusAnalyzerProject/Resources/IndiaStateCensusData.csv") == 29)
   }
 
-  test("test_InputFileIsWrong_ReturnIncorrectFileException") {
+  test("test_InputFilePathIsWrong_ReturnIncorrectFilePathException") {
     val thrown = intercept[Exception] {
     val objCensus = new CensusAnalyzer()
     objCensus.loadCSVData("./src/scala/CensusAnalyzerProject/Resources/IndiaStateCensusData.csv")
     }
-    assert(thrown.getMessage == CensusAnalyzerExceptionEnum.inCorrectFile.toString)
+    assert(thrown.getMessage == CensusAnalyzerExceptionEnum.inCorrectPath.toString)
+  }
 
+  test("test_InputFileTypeIsWrong_ReturnIncorrectFileException") {
+    val thrown = intercept[Exception] {
+      val objCensus = new CensusAnalyzer()
+      assert(objCensus.loadCSVData("./src/main/scala/CensusAnalyzerProject/Resources/IndiaStateCensusData.txt") == CensusAnalyzerExceptionEnum.inCorrectFile.toString)
+    }
   }
 }
