4a4bec5377b0e824ff6f760702459d29c67e8aba
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-08-31 14:05:46 +0530

    Niraj[Added]:Scala source codes of Census Analyzer for UserCase 2

--- SourceCodes/CensusAnalyzer.scala
+++ SourceCodes/CensusAnalyzer.scala
@@ -2,22 +2,24 @@ package CensusAnalyzerProject
 
 class CensusAnalyzer {
 
-  def loadCSVData(filePath:String): Int ={
+  def loadCSVDataIndiaStateCensus(filePath:String): Int = {
     try {
       if(!filePath.toLowerCase.endsWith(".csv")) {
         throw new CensusAnalyzerException(CensusAnalyzerExceptionEnum.inCorrectFile)
       }
       val fileReader = io.Source.fromFile(filePath)
       var countRows = 0
+      var colsLength = 0
       for(line <- fileReader.getLines()) {
         val cols = line.split(",").map(_.trim)
-        if(cols.length != 4) {
-          throw new CensusAnalyzerException(CensusAnalyzerExceptionEnum.inCorrectDelimiter)
-        }
-        if(countRows == 0){
+        if(countRows == 0) {
+          colsLength = cols.length
           if(cols(0).toLowerCase != "state" || cols(1).toLowerCase != "population" || cols(2).toLowerCase != "areainsqkm" || cols(3).toLowerCase != "densitypersqkm" ) {
             throw new CensusAnalyzerException(CensusAnalyzerExceptionEnum.inCorrectFields)
           }
+        }
+        if(cols.length != colsLength) {
+          throw new CensusAnalyzerException(CensusAnalyzerExceptionEnum.inCorrectDelimiter)
         }
         countRows += 1
       }
@@ -26,5 +28,35 @@ class CensusAnalyzer {
     catch {
       case ex:java.io.FileNotFoundException => throw new CensusAnalyzerException(CensusAnalyzerExceptionEnum.inCorrectPath)
     }
+  }
+
+  def loadCSVDataIndiaStateCode(filePath:String):Int = {
+    try {
+      if(!filePath.toLowerCase.endsWith(".csv")) {
+        throw new CensusAnalyzerException(CensusAnalyzerExceptionEnum.inCorrectFile)
+      }
+      val fileReader = io.Source.fromFile(filePath)
+      var countRows = 0
+      var colsLength = 0
+      for(line <- fileReader.getLines()) {
+        val cols = line.split(",").map(_.trim)
+        if(countRows == 0) {
+          colsLength = cols.length
+          if(cols(1).toLowerCase != "state name" || cols(3).toLowerCase != "statecode") {
+            throw new CensusAnalyzerException(CensusAnalyzerExceptionEnum.inCorrectFields)
+          }
+        }
+        if(cols.length != colsLength) {
+          throw new CensusAnalyzerException(CensusAnalyzerExceptionEnum.inCorrectDelimiter)
+        }
+        countRows += 1
+      }
+
+      countRows - 1
+    }
+    catch {
+      case ex:java.io.FileNotFoundException => throw new CensusAnalyzerException(CensusAnalyzerExceptionEnum.inCorrectPath)
+    }
+
   }
 }
